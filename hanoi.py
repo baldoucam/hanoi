44a0556213262b7735fa1ca37ec31ac92e0276d5
baldo <23262618@AULAS.UCAM.EDU>
2017-02-08 17:17:07 +0100

    hola

--- hanoi.py
+++ hanoi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+﻿#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -17,6 +17,7 @@ moves = 0
 """
     
     Este sencillo algoritmo recurrente resuelve el problema de las torres de Hanoi.
+    hola
 
 """
 
